3907fbe4eb694d484bf92625a9b73a26e763074e
nurico vicyyanto <nuricovicyyanto2@gmail.com>
2022-01-02 10:02:29 +0700

    s

---
+++ imgmatch.py
@@ -0,0 +1,69 @@
+import cv2
+import numpy as np
+import face_recognition
+import os
+
+hand_cascade = cv2.CascadeClassifier('hand_cascade.xml')
+
+count = 0
+title = 'Face Detection'
+path = 'image'
+images = []
+classNames = []
+cek = os.path.exists(path)
+myList = os.listdir(path)
+for cl in myList:
+    curImg = cv2.imread(f'{path}/{cl}')
+    images.append(curImg)
+    classNames.append(os.path.splitext(cl)[0])
+
+def findEncodings(images):
+    encodeList = []
+    for img in images:
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        encode = face_recognition.face_encodings(img)[0]
+        encodeList.append(encode)
+    return encodeList
+
+encodeListKnown = findEncodings(images)
+
+cap = cv2.VideoCapture(0)
+
+while True:
+    ret, img = cap.read()
+    img = cv2.flip(img, 1)
+
+    imgS = cv2.resize(img,(0,0),None,0.25,0.25)
+    imgS = cv2.cvtColor(imgS, cv2.COLOR_BGR2RGB)
+    facesCurFrame = face_recognition.face_locations(imgS)
+    encodesCurFrame = face_recognition.face_encodings(imgS,facesCurFrame)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    objeck = hand_cascade.detectMultiScale(gray, 1.1, 4)
+
+    for encodeFace,faceLoc in zip(encodesCurFrame,facesCurFrame):
+        matches = face_recognition.compare_faces(encodeListKnown,encodeFace)
+        faceDis = face_recognition.face_distance(encodeListKnown,encodeFace)
+        matchIndex = np.argmin(faceDis)
+
+        if matches[matchIndex]:
+            name = classNames[matchIndex].upper()
+            y1,x2,y2,x1 = faceLoc
+            y1, x2, y2, x1 = y1*4,x2*4,y2*4,x1*4
+            cv2.rectangle(img,(x1,y1),(x2,y2),(0,255,0),2)
+            cv2.rectangle(img,(x1,y2-35),(x2,y2),(0,255,0),cv2.FILLED)
+            cv2.putText(img,name,(x1+6,y2-6),cv2.FONT_HERSHEY_COMPLEX,1,(255,255,255),2)
+
+    cv2.imshow(title, img)
+    
+    keyCode = cv2.waitKey(1)
+    if keyCode%256 == 27:
+        # ESC pressed
+        print("Escape hit, closing...")
+        break
+    elif keyCode%256 == 32:
+        # SPACE pressed
+        file = 'C:/Users/vicky/Documents/Opencv/templatematch/image/user'+str(count)+'.jpg'
+        cv2.imwrite(file, img)
+        count +=1
+        break
+        
